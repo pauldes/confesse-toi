b8305767fed53be1f4ad1ce4b6dc3ce682b7786e
pauldes <paul.desigaud@insa-lyon.fr>
2017-10-29 15:33:57 +0100

    db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://AdminOfSins:PASSWORD@clusterofsins-shard-00-00-iaate.mongodb.net:27017,clusterofsins-shard-00-01-iaate.mongodb.net:27017,clusterofsins-shard-00-02-iaate.mongodb.net:27017/test?ssl=true&replicaSet=ClusterOfSins-shard-0&authSource=admin',
-    localUrl: 'mongodb://AdminOfSins:PASSWORD@clusterofsins-shard-00-00-iaate.mongodb.net:27017,clusterofsins-shard-00-01-iaate.mongodb.net:27017,clusterofsins-shard-00-02-iaate.mongodb.net:27017/ClusterOfSins?ssl=true&replicaSet=ClusterOfSins-shard-0&authSource=admin'
+    remoteUrl : 'mongodb://AdminOfSins:S3cretOfSins@clusterofsins-shard-00-00-iaate.mongodb.net:27017,clusterofsins-shard-00-01-iaate.mongodb.net:27017,clusterofsins-shard-00-02-iaate.mongodb.net:27017/test?ssl=true&replicaSet=ClusterOfSins-shard-0&authSource=admin',
+    localUrl: 'mongodb://AdminOfSins:S3cretOfSins@clusterofsins-shard-00-00-iaate.mongodb.net:27017,clusterofsins-shard-00-01-iaate.mongodb.net:27017,clusterofsins-shard-00-02-iaate.mongodb.net:27017/ClusterOfSins?ssl=true&replicaSet=ClusterOfSins-shard-0&authSource=admin'
 };

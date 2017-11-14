9d045b334063f5b361138d4521086958628d339a
pauldes <paul.desigaud@insa-lyon.fr>
2017-11-14 08:34:44 +0100

    Lien user-sin

--- app/models/userxsin.js
+++ app/models/userxsin.js
@@ -1,14 +1,33 @@
 var mongoose = require('mongoose');
 
+/*
+ * Model used to
+ */
+
 module.exports = mongoose.model('User_x_Sin', {
     userId: {
         type: String,
         default: ''
     },
+
+    /*
     voteType: {
         type: Number,
         default: 0
     },
+    */
+    voteType: {
+        upvote: {
+            type: Boolean,
+            default: 0
+        },
+        downvote: {
+            type: Boolean,
+            default: 0
+        }
+    },
+
+
     sinId: {
         type: String,
         default: ''
